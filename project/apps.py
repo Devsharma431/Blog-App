acf6b26239d97bcfdf3e4197c1d62740b0f8f7c9
Dev sharma <97778403+Devsharma431@users.noreply.github.com>
2022-11-25 21:49:28 +0530

    Add files via upload

---
+++ project/apps.py
@@ -0,0 +1,14 @@
+# <<<<<<< HEAD
+from django.apps import AppConfig
+
+
+class ProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'project'
+
+from django.apps import AppConfig
+
+
+class ProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'project'
