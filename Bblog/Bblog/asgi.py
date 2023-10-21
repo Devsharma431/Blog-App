bb4ec203f96c047c6e6241f31babe2699055ed1c
Devsharma431 <devpython345898@gmail.com>
2023-10-21 22:46:46 +0530

    add project to the app

---
+++ Bblog/Bblog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Bblog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bblog.settings')
+
+application = get_asgi_application()
